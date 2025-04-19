5777ca09e097f01626716b2f77324cdc0dfb31af
the0nec0der <yusufekici149@gmail.com>
2025-04-19 13:32:04 +0300

    feat: assign producible soldier units to building assets and update related data

--- Assets/Scripts/Gameplay/Buildings/BuildingData.cs
+++ Assets/Scripts/Gameplay/Buildings/BuildingData.cs
@@ -9,8 +9,7 @@ namespace Gameplay.Buildings
     {
         [Header("Building Data")]
         [SerializeField] private string buildingName;
-        [SerializeField] private string purpose;
-        [SerializeField] private string notes;
+        [SerializeField, TextArea(2, 4)] private string description;
         [SerializeField] private Sprite buildingSprite;
         [SerializeField] private Vector2Int size;
         [SerializeField] private int health;
@@ -20,7 +19,7 @@ namespace Gameplay.Buildings
         [SerializeField] private SoldierData[] producibleSoldiers;
 
         public string BuildingName => buildingName;
-        public string Purpose => purpose;
+        public string Description => description;
         public Sprite Sprite => buildingSprite;
         public Vector2Int Size => size;
         public int Health => health;
@@ -28,5 +27,7 @@ namespace Gameplay.Buildings
 
         public SoldierData[] ProducibleSoldiers => producibleSoldiers;
         public bool CanProduceSoldiers => producibleSoldiers != null && producibleSoldiers.Length > 0;
+
+        [Space, SerializeField, TextArea(4, 4)] private string notes;
     }
 }
