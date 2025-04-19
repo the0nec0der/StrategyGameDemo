0f4d04be375e5803a4b1ab6423b72a3d03b30727
the0nec0der <yusufekici149@gmail.com>
2025-04-19 13:32:04 +0300

    feat: assign producible soldier units to building assets and update related data

--- Assets/Scripts/Gameplay/Solider Units/SoliderData.cs
+++ Assets/Scripts/Gameplay/Solider Units/SoliderData.cs
@@ -6,15 +6,19 @@ namespace Gameplay.SoldierUnits
     public class SoldierData : ScriptableObject
     {
         [SerializeField] private string soldierName;
+        [SerializeField, TextArea(2, 4)] private string description;
         [SerializeField] private int damage;
         [SerializeField] private int health = 10;
         [SerializeField] private Sprite sprite;
         [SerializeField] private GameObject prefab;
 
         public string SoldierName => soldierName;
+        public string Description => description;
         public int Damage => damage;
         public int Health => health;
         public Sprite Sprite => sprite;
         public GameObject Prefab => prefab;
+
+        [Space, SerializeField, TextArea(4, 4)] private string notes;
     }
 }
