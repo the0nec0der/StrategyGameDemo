aee6cc810b5e6d30ef5d25351991c701aba7e456
the0nec0der <yusufekici149@gmail.com>
2025-04-19 23:26:24 +0300

    feat: add GameLogicMediator to centralize core system access and data loading

---
+++ Assets/Scripts/Gameplay/GameLogicMediator.cs
@@ -0,0 +1,55 @@
+using Core.InstanceSystem;
+
+using SaveSystem;
+
+using SettingSystem;
+
+using UI;
+
+using UnityEngine;
+
+public class GameLogicMediator : MonoBehaviour
+{
+    public static GameLogicMediator Instance => Instanced<GameLogicMediator>.Instance;
+
+    public SaveManager SaveManager { get; private set; }
+    public AudioManager AudioManager { get; private set; }
+
+    public SettingsMenuController SettingsMenuController { get; private set; }
+
+    private void Start()
+    {
+        if (Instance != this)
+            return;
+
+        SaveManager = Instanced<SaveManager>.Instance;
+        AudioManager = Instanced<AudioManager>.Instance;
+
+        SettingsMenuController = Instanced<SettingsMenuController>.Instance;
+
+        LoadGameData();
+    }
+
+    private void OnDestroy()
+    {
+        SaveGameData();
+    }
+
+    private void LoadGameData()
+    {
+        try
+        {
+            SaveManager?.LoadGame();
+        }
+        catch (SaveManager.SaveNotFoundException)
+        {
+            Debug.LogWarning("Save file not found. Starting with default values.");
+            // TODO: Initialize default values here.
+        }
+    }
+
+    private void SaveGameData()
+    {
+        SaveManager?.SaveGame();
+    }
+}
